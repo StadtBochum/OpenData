4cacb361ea37cd48c957b2bb11735b24ce50dd24
Philipp Zuber <philipp.zuber@posteo.de>
2024-05-04 11:22:38 +0200

    Update collect_ecowitt.py

--- collector/collect_ecowitt.py
+++ collector/collect_ecowitt.py
@@ -1,3 +1,6 @@
+# Daily collector for ecowitt gw2001 (smart benches bochum)
+
+# Import libs
 import sys
 import os
 from datetime import datetime, timedelta
@@ -9,9 +12,10 @@ from utils.utils_collect_ecowitt import load_device_list, \
     write_dataframes_to_csv, aggregate_daily_to_monthly, \
     load_historic_data_for_date, aggregate_monthly_to_yearly
 
+
+# Set variables
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 data_dir = os.path.join(os.getcwd(), "data/ENVI/ecowitt_gw2001")
-
 berlin_timezone = pytz.timezone('Europe/Berlin')
 current_time_berlin = datetime.now(berlin_timezone)
 end_of_yesterday = current_time_berlin.replace(hour=0, minute=0, second=0, microsecond=0)  # Midnight at the start of today
@@ -19,23 +23,31 @@ start_of_yesterday = end_of_yesterday - timedelta(days=1)  # Midnight at the sta
 start_of_yesterday_str = start_of_yesterday.strftime('%Y-%m-%d %H:%M:%S')
 end_of_yesterday_str = end_of_yesterday.strftime('%Y-%m-%d %H:%M:%S')
 
-ecowitt_gw2001_list = load_device_list()
-#print(smart_benches)
 
+# Load devices
+ecowitt_gw2001_list = load_device_list()
+print(ecowitt_gw2001_list)
+
+
+# Create dirs
 create_directories_for_devices(data_dir, ecowitt_gw2001_list)
 
-#data_yesterday = load_historic_data_for_days(ecowitt_gw2001_list)
-#write_dataframes_to_csv(data_yesterday, data_dir)
 
-start_date = "2024-04-01"
-end_date = "2024-04-09"
+# Download historic data
+#start_date = "2024-04-01"
+#end_date = "2024-04-09"
+#dates_to_process = pd.date_range(start=start_date, end=end_date, freq='D').strftime('%Y-%m-%d')
+#if True == True:
+#    for date in dates_to_process:
+#        data_specific = load_historic_data_for_date(ecowitt_gw2001_list, date)
+#        write_dataframes_to_csv(data_specific, data_dir)
 
-dates_to_process = pd.date_range(start=start_date, end=end_date, freq='D').strftime('%Y-%m-%d')
 
-if True == True:
-    for date in dates_to_process:
-        data_specific = load_historic_data_for_date(ecowitt_gw2001_list, date)
-        write_dataframes_to_csv(data_specific, data_dir)
+# Get yesterdays weather data for every device
+data_yesterday = load_historic_data_for_days(ecowitt_gw2001_list)
+write_dataframes_to_csv(data_yesterday, data_dir)
 
+
+# Aggregate data from singlle csv files
 aggregate_daily_to_monthly(data_dir)
 aggregate_monthly_to_yearly(data_dir)
